ef391a49badf65b4e3ca1dc9f893d690dcacdc20
Shimul dev nath <shimuldevnath330@gmail.com>
2025-09-17 13:26:06 +0600

    added blogPreview section

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import CommunityHighlight from "./components/CommunityHighlight";
 import FeaturedCities from "./components/FeaturedCities";
 import HowItWorks from "./components/HowItWorks";
 import Contact from "./components/Contact";
+import BlogPreview from "./components/BlogPreview";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
     <CommunityHighlight></CommunityHighlight>
     <FeaturedCities></FeaturedCities>
     <HowItWorks></HowItWorks>
+    <BlogPreview></BlogPreview>
     <Contact></Contact>
    </div>
   );
