9c945f47a9511d5eaca549bdbf4e6b173a1dc48d
unknown <mdfaruk01720875872>
2025-09-18 14:42:16 +0600

    added an newsleter signup setup section

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Banner from "./components/Banner";
 import CommunityHighlight from "./components/CommunityHighlight";
 import FeaturedCities from "./components/FeaturedCities";
+import NewsletterSignup from "./components/NewsletterSignup";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
     <Banner></Banner>
     <CommunityHighlight></CommunityHighlight>
     <FeaturedCities></FeaturedCities>
+    <NewsletterSignup></NewsletterSignup>
    </div>
   );
 }
