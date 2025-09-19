e848bbfbe85657d59c3b38b0b479231d9a9739d5
unknown <mdfaruk01720875872>
2025-09-19 09:44:21 +0600

    added an explore cities page setup

---
+++ src/app/page/ExploreCities.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExploreCities = () => {
+    return (
+        <div>
+            <h1>this is ExploreCities</h1>
+        </div>
+    );
+};
+
+export default ExploreCities;
