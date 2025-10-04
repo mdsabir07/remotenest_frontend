dcf235b1b8619ef17ca404835e5969da3f44ef7c
unknown <mdfaruk01720875872>
2025-10-04 21:23:40 +0600

    added an dashboard to home page button

--- src/components/DashboardSidebar.jsx
+++ src/components/DashboardSidebar.jsx
@@ -96,6 +96,10 @@ export default function DashboardSidebar({ sidebarOpen, setSidebarOpen }) {
             </nav>
 
             <div className="mt-auto space-y-2">
+                {/* dashboard to home */}
+                <Link href="/" className='w-full flex items-center justify-center gap-2 px-4 py-2 bg-gray-100 dark:bg-gray-700 rounded-md hover:bg-gray-200 dark:hover:bg-gray-600 transition'>
+                Go to Home
+                </Link>
                 <button
                     onClick={toggleTheme}
                     className="w-full flex items-center justify-center gap-2 px-4 py-2 bg-gray-100 dark:bg-gray-700 rounded-md hover:bg-gray-200 dark:hover:bg-gray-600 transition"
